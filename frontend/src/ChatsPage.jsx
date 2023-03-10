ae79b27612332b1258f554ba56134c1a5a2334b8
Enky <enkh6at@yahoo.com>
2023-03-10 11:31:54 +0000

    finished

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,39 @@
-const ChatsPage = () => {
-  return <div className="background">chats...</div>;
+// import {
+//   MultiChatSocket,
+//   MultiChatWindow,
+//   useMultiChatLogic,
+// } from "react-chat-engine-advanced";
+
+// const ChatsPage = (props) => {
+//   const chatProps = useMultiChatLogic(
+//     "cdfe83ae-b412-45d1-ab62-9c40aa188ed6",
+//     props.user.username,
+//     props.user.secret
+//   );
+//   return (
+//     <div className="background">
+//       <MultiChatSocket {...chatProps} />
+//       <MultiChatWindow {...chatProps} />
+//     </div>
+//   );
+// };
+// export default ChatsPage;
+
+// cdfe83ae-b412-45d1-ab62-9c40aa188ed6
+
+import React from 'react';
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
 };
+
 export default ChatsPage;
