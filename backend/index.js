a9ec7e8e6f935ef6e1e6e83930c6a1c46d4dc6e7
Enky <enkh6at@yahoo.com>
2023-03-10 02:18:09 +0000

    backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "b4ab5d1c-5ece-4d7e-af64-203a10a3b6ee"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+    
+  }
+//   b4ab5d1c-5ece-4d7e-af64-203a10a3b6ee
+});
+
+app.listen(3001);
